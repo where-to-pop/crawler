d5841b3f6fa94346e2c3a1426c90ee53fb3f9126
Dahye Won <doall1102@gmail.com>
2025-04-16 09:52:38 +0900

    feat: run seoul_api_fetcher from main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from src.popply_crawler import PopplyCrawler
+from sources import PopplyCrawler
+from sources import SeoulApiFetcher
+from dotenv import load_dotenv
+import os
 
 def main():
-    crawler = PopplyCrawler()
-    try:
-        crawler.crawl_popups()
-        crawler.save_to_json()
-    finally:
-        crawler.quit()
+    load_dotenv()  # 이거 위치가 여기가 괜찮으려나...
+
+    SEOUL_DATA_API_KEY = os.environ.get("SEOUL_DATA_API_KEY")
+    api_fetcher = SeoulApiFetcher(SEOUL_DATA_API_KEY)
+
+    test_area_code = "POI068"
+    api_fetcher.fetch_population(test_area_code)
+
 
 if __name__ == "__main__":
     main()
